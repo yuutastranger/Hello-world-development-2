0080515ed433a5b8184f47dd48de0c27f27f264a
Daniil <yuutaprod@gmail.com>
2026-04-01 00:32:34 +0300

    Added a function of fibonacci digits

--- i_want_to_sleep.cpp
+++ i_want_to_sleep.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+int fib(int n){
+    if (n <= 2){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
 int main(){
+    int n; cout << "Insert your n = "; cin >> n;
+    cout << fib(n) << endl;
     cout << "Hello world";
     return 0;
 }
