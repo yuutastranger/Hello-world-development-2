1af7f9728b874cd46bab201b8f236cbfb33fbdf8
Daniil <yuutaprod@gmail.com>
2026-04-01 00:37:28 +0300

    Fibonachi func fixed to iteration version. Print all numerics up to input number parameter

--- i_want_to_sleep.cpp
+++ i_want_to_sleep.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 using namespace std;
-int fib(int n){
-   if (n == 1){
-        return 0;
+void fib(int n){
+    int a1 = 0; int a2 = 1;
+    if (n == 0){
+        cout << "Error" << endl;
     }
-    if (n == 2){
-        return 1;
+    if (n == 1){
+        cout << a1 << endl;
     }
-    return fib(n-1) + fib(n-2);
+    if (n >= 2){
+        cout << a1 << endl << a2 << endl;
+    }
+    for (int i = 2; i < n; i++){
+        int a = a1 + a2;
+        cout << a << endl;
+        a1 = a2;
+        a2 = a;
+    }
 }
 int main(){
     int n; cout << "Insert your n = "; cin >> n;
-    cout << fib(n) << endl;
+    fib(n);
     cout << "Hello world";
     return 0;
 }
